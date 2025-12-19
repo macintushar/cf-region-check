49d6f3489d00bf4244d6b1c3f181f77b0572f6a5
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-12-20 04:46:02 +0530

    new

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,23 @@ app.get('/', async (c) => {
 
   const db = drizzle(c.env.MY_DB);
   const headers = c.req.raw.headers.entries()
+  const headers2 = await c.req.raw.headers.entries()
   console.log(JSON.stringify(headers))
+  console.log(JSON.stringify(headers2))
+
+  const fin = {
+    "h1": JSON.stringify(headers),
+    "h2": JSON.stringify(headers2),
+  }
 
   db.insert(loggingTable).values({
-    headers: JSON.stringify(headers),
+    headers: JSON.stringify(fin),
     name: 'test',
     timestamp: new Date(),
   })
 
   const result = await db.insert(loggingTable).values({
-    headers: JSON.stringify(headers),
+    headers: JSON.stringify(fin),
     name: 'test',
     timestamp: new Date(),
   })
