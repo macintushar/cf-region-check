3f9394a60eeaef90d651981fd0dda6584e8457db
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-12-20 04:35:01 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = new Hono<{ Bindings: Bindings }>()
 app.get('/', async (c) => {
 
   const db = drizzle(c.env.MY_DB);
-  const headers = c.req.header
+  const headers = c.req.header('CF-Region')
   console.log(headers)
 
   db.insert(loggingTable).values({
